8810915523d3f3094d087f1311ce60ce443c50e0
Haidar Zamzam <haidar.zamzam99@gmail.com>
2020-01-21 19:39:20 +0700

    Initial Commit

---
+++ app/src/main/java/com/haidev/android_mvvm_pattern/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.haidev.android_mvvm_pattern
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
